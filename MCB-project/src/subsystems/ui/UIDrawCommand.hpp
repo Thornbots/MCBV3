69aed5ea215fc46cb314b327c63ca9f3ccb56686
rhit-hardenej <hardenej@rose-hulman.edu>
2025-05-12 21:05:02 -0400

    commented out wip laneassistlines and peekinglines, for pr into main

--- MCB-project/src/subsystems/ui/UIDrawCommand.hpp
+++ MCB-project/src/subsystems/ui/UIDrawCommand.hpp
@@ -33,20 +33,20 @@ public:
 
         // addGraphicsObject(&testGraphics);
         // addGraphicsObject(&testFill);
-        addGraphicsObject(&laneAssistLines);
+        // addGraphicsObject(&laneAssistLines);
         addGraphicsObject(&supercapChargeIndicator);
         addGraphicsObject(&chassisOrientationIndicator);
-        addGraphicsObject(&peekingLines);
+        // addGraphicsObject(&peekingLines);
 
     };
 
     void initialize() override { ui->setTopLevelContainer(this); };
 
     void execute() override {
-        laneAssistLines.update();
+        // laneAssistLines.update();
         supercapChargeIndicator.update();
         chassisOrientationIndicator.update();
-        peekingLines.update();
+        // peekingLines.update();
     };
 
     //ui subsystem won't do anything until its top level container is set, so we are ok to add objects to the command in the constructor
@@ -66,9 +66,9 @@ private:
     // add top level graphics objects here and in the constructor
     // TestGraphics testGraphics{};
     // TestFill testFill{};
-    LaneAssistLines laneAssistLines{gimbal};
+    // LaneAssistLines laneAssistLines{gimbal};
     SupercapChargeIndicator supercapChargeIndicator{drivetrain};
     ChassisOrientationIndicator chassisOrientationIndicator{gimbal};
-    PeekingLines peekingLines{drivetrain, gimbal};
+    // PeekingLines peekingLines{drivetrain, gimbal};
 };
 }  // namespace commands
