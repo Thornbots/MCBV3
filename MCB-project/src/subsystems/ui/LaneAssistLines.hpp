15a46151b332b0aad8dde0b19e0cedaad04f29b1
rhit-hardenej <hardenej@rose-hulman.edu>
2025-05-28 11:40:37 -0400

    change to laneassistlines, untested

--- MCB-project/src/subsystems/ui/LaneAssistLines.hpp
+++ MCB-project/src/subsystems/ui/LaneAssistLines.hpp
@@ -21,53 +21,38 @@ public:
     void update() {
         float pitch = gimbal->getPitchEncoderValue();
         //vs's in robot space, don't need to rotate with drivetrain, does need to rotate with pitch
-        Vector3d vs[4];
-        vs[0] = Vector3d{ROBOT_RADIUS, FORWARD_DISTANCE, 0};
-        vs[1] = Vector3d{ROBOT_RADIUS, FORWARD_DISTANCE - ROBOT_RADIUS, 0};
-        vs[2] = Vector3d{-ROBOT_RADIUS, FORWARD_DISTANCE, 0};
-        vs[3] = Vector3d{-ROBOT_RADIUS, FORWARD_DISTANCE - ROBOT_RADIUS, 0};
-        Vector3d temp;
-        Vector3d temp2;
-        Vector2d temp3;
-        float xPositions[4];
-        float yPositions[4];
-        for(int i=0; i<4; i++){
-            //currently  we are in robot space, need to get to pivot
-            temp2 = Projections::robotSpaceToPivotSpace(vs[i]);
-            //now in pivot, rotate by pitch
-            temp = temp2.rotatePitch(pitch);
-            //now that we rotated around pivot, go to vtm
-            temp2 = Projections::pivotSpaceToVtmSpace(temp);
-            //now get to screen space
-            temp3 = Projections::vtmSpaceToScreenSpace(temp2);
+        Vector3d rightTop3 = Vector3d{ROBOT_RADIUS, FORWARD_DISTANCE, 0};
+        Vector3d rightRef3 = Vector3d{ROBOT_RADIUS, FORWARD_DISTANCE - ROBOT_RADIUS, 0};
+        Vector3d leftTop3 = Vector3d{-ROBOT_RADIUS, FORWARD_DISTANCE, 0};
+        Vector3d leftRef3 = Vector3d{-ROBOT_RADIUS, FORWARD_DISTANCE - ROBOT_RADIUS, 0};
 
-
-            // if(i%2==0){
-            //     //0 or 2, put into x and y as is unless y is less than 0, if it is then stop this loop and not show any lines
-            //     if(temp3.getY()<0){
-            //         //would mean that the entire line would be below the screen, so don't try to calculate the other point
-            //         this->hide(); //hide all the lines I contain
-            //         return;
-            //     }
-                xPositions[i] = temp3.getX();
-                yPositions[i] = temp3.getY();
-            // } else {
-            //     //1 or 3, x and y get extended to the bottom of the screen
-            //     xPositions[i] = xPositions[i-1] - (yPositions[i-1])*(xPositions[i-1]-xPositions[i])/(yPositions[i-1]-yPositions[i]);
-            //     yPositions[i] = 0;
-            // }
+        Vector2d rightTop2 = project(rightTop3, pitch);
+        if(rightTop2.getY()<0){
+            //if top line is off the bottom of the screen, don't project the other points and draw nothing
+            this->hide(); //hide all the lines I contain
+            return;
         }
 
+        Vector2d rightRef2 = project(rightRef3, pitch);
+        Vector2d leftTop2 = project(leftTop3, pitch);
+        Vector2d leftRef2 = project(leftRef3, pitch);
+
+        Vector2d rightBottom2 = getBottom(rightTop2, rightRef2);
+        Vector2d leftBottom2 = getBottom(leftTop2, leftRef2);
+
         this->show(); //show all the lines I contain
-        right.x1 = xPositions[0];
-        right.x2 = xPositions[1];
-        left.x1 = xPositions[2];
-        left.x2 = xPositions[3];
+
+        right.x1 = rightTop2.getX();
+        right.y1 = rightTop2.getY();
+
+        right.x2 = rightBottom2.getX();
+        right.y2 = rightBottom2.getY();
+
+        left.x1 = leftTop2.getX();
+        left.y1 = leftTop2.getY();
         
-        right.y1 = yPositions[0];
-        right.y2 = yPositions[1];
-        left.y1 = yPositions[2];
-        left.y2 = yPositions[3];
+        left.x2 = leftBottom2.getX();
+        left.y2 = leftBottom2.getY();
     }
 
 
@@ -88,8 +73,28 @@ static constexpr float FORWARD_DISTANCE = 1.2; //meters, how far forward the lan
 
     static constexpr uint16_t THICKNESS = 2;  // pixels
 
-    GimbalSubsystem* gimbal = nullptr;
+    GimbalSubsystem* gimbal;
 
     Line left{UISubsystem::Color::CYAN, 0, 0, 0, 0, THICKNESS};
     Line right{UISubsystem::Color::CYAN, 0, 0, 0, 0, THICKNESS};
+
+    Vector2d project(Vector3d in, float pitch){
+        Vector3d temp;
+        Vector3d temp2;
+        
+        //currently  we are in robot space, need to get to pivot
+        temp2 = Projections::robotSpaceToPivotSpace(in);
+        //now in pivot, rotate by pitch
+        temp = temp2.rotatePitch(pitch);
+        //now that we rotated around pivot, go to vtm
+        temp2 = Projections::pivotSpaceToVtmSpace(temp);
+        //now get to screen space
+        return Projections::vtmSpaceToScreenSpace(temp2);
+    }
+
+    Vector2d getBottom(Vector2d top, Vector2d ref){
+        Vector2d diff = ref-top;
+        float slope = diff.getY()/diff.getX();
+        return Vector2d(top.getX() - top.getY() / slope, 0);
+    }
 };
