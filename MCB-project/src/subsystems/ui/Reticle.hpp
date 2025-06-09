f81488f03ebee9422f631f8dbe212b058bbe7449
rhit-hardenej <hardenej@rose-hulman.edu>
2025-06-09 19:34:17 -0400

    add another enum for reticle

--- MCB-project/src/subsystems/ui/Reticle.hpp
+++ MCB-project/src/subsystems/ui/Reticle.hpp
@@ -39,13 +39,13 @@ public:
         //initialPos and initialVelo are in pivot space, parallel to the ground so gravity is pulling in the -z direction only
         
         //NUM_THINGS applies with rectangles and lines (so far, other modes may be added)
-        if(mode==ReticleMode::RECTANGLES || mode==ReticleMode::LINES)
+        if(mode==ReticleDrawMode::RECTANGLES || mode==ReticleDrawMode::LINES)
             for(int i=0;i<NUM_THINGS;i++){
                 float t = (DISTANCES[i] - initialPos.getY()) / initialVelo.getY(); //(distance to travel [meters]) divided by (speed to get there [meters/seconds]) gives (time to get there [seconds])
                 float zFinal = initialPos.getZ() + initialVelo.getZ() * t - tap::algorithms::ACCELERATION_GRAVITY/2 * t * t; //make sure gravity is negative, the taproot constant is positive, need to subtract
                 Vector3d landingSpot{initialPos.getX(), DISTANCES[i], zFinal}; //side to side doesn't change, we are defining the down range distance, and we calculated the height off the ground
 
-                if(mode==ReticleMode::RECTANGLES){
+                if(mode==ReticleDrawMode::RECTANGLES){
                     //need the edges because the corners will trace a trapezoid
                     Vector2d right = project(landingSpot + panelEdges[0], pitch); //gives width by right x minus left x
                     Vector2d left = project(landingSpot + panelEdges[1], pitch); //gives x of rect
@@ -75,12 +75,20 @@ public:
     }
 
 private:
-    enum class ReticleMode : uint8_t
+    enum class ReticleDrawMode : uint8_t
     {
         RECTANGLES = 0, // multiple rectangles of different colors, tops and bottoms of them are tops and bottoms of panels, midpoints of sides are edges of standard size panels
         LINES = 1,      // multiple of horizontal lines, widths represent panel widths, heights represent where the center of a standard panel needs to be to hit it, lines go across the center of a panel, endpoints are edges of panels
-        // CURVES = 2,     // two curves that act like the endpoints of a lot of LINES (see above)
     };
+
+    enum class ReticleSolveMode : uint8_t
+    {
+        FOR_HEIGHT_OFF_GROUND = 0, // uses pitch and DISTANCES away to solve for height, redraws the reticle often because pitch changes often
+        FOR_PITCH = 1,             // uses AVERAGE_HEIGHT_OFF_GROUND and DISTANCES away to solve for pitch, reticle doesn't ever need redrawn because constants are constant
+        FOR_DISTANCE_AWAY = 2      // uses pitch and AVERAGE_HEIGHT_OFF_GROUND to solve for distance away, redraws the reticle often because pitch changes often
+    };
+
+    static constexpr float AVERAGE_HEIGHT_OFF_GROUND = 0.07; //meters, center of panel to ground, for calculating where on screen reticle things should be
 
     static constexpr float PANEL_WIDTH = 0.135; //meters
     static constexpr float PANEL_HEIGHT = 0.125; //meters, height across the surface
@@ -101,13 +109,13 @@ private:
     // GraphicsContainer curvesContainer{};
     GimbalSubsystem* gimbal;
 
-    ReticleMode mode = ReticleMode::RECTANGLES;
+    ReticleDrawMode mode = ReticleDrawMode::RECTANGLES;
 
     
     //for rectangles and lines
-    static constexpr int NUM_THINGS = 1;
-    static constexpr float DISTANCES[NUM_THINGS] = {12/*, 2, 3, 4, 5, 6*/}; //y distances, in meters
-    static constexpr UISubsystem::Color COLORS[NUM_THINGS] = {UISubsystem::Color::PURPLISH_RED/*, UISubsystem::Color::PINK, UISubsystem::Color::ORANGE, UISubsystem::Color::YELLOW, UISubsystem::Color::GREEN, UISubsystem::Color::CYAN*/};
+    static constexpr int NUM_THINGS = 6;
+    static constexpr float DISTANCES[NUM_THINGS] = {1, 2, 3, 4, 5, 6}; //y distances, in meters
+    static constexpr UISubsystem::Color COLORS[NUM_THINGS] = {UISubsystem::Color::PURPLISH_RED, UISubsystem::Color::PINK, UISubsystem::Color::ORANGE, UISubsystem::Color::YELLOW, UISubsystem::Color::GREEN, UISubsystem::Color::CYAN};
 
     UnfilledRectangle rects[NUM_THINGS];
     Line lines[NUM_THINGS];
@@ -116,9 +124,9 @@ private:
         //maybe make it so that objects at the same index have the same graphics name, so instead of hide one show another replace
         //would require an ExclusiveContainer or SelectionContainer, each has one graphics name and many graphics objects, and you can set which is selected
         //but for now assume reticle doesn't change mode very often (if ever)
-        rectsContainer.setHidden(mode!=ReticleMode::RECTANGLES); //hidden if not rects
-        linesContainer.setHidden(mode!=ReticleMode::LINES); //hidden if not lines
-        // curvesContainer.setHidden(mode!=ReticleMode::CURVES);
+        rectsContainer.setHidden(mode!=ReticleDrawMode::RECTANGLES); //hidden if not rects
+        linesContainer.setHidden(mode!=ReticleDrawMode::LINES); //hidden if not lines
+        // curvesContainer.setHidden(mode!=ReticleDrawMode::CURVES);
     }
 
     Vector2d project(Vector3d in, float pitch){
