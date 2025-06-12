91b7f96110a58b0e6cc2eaf8b05b8e01e648b6af
rhit-hardenej <hardenej@rose-hulman.edu>
2025-06-12 18:44:46 -0400

    sentry ref message

--- MCB-project/src/subsystems/jetson/JetsonSubsystem.hpp
+++ MCB-project/src/subsystems/jetson/JetsonSubsystem.hpp
@@ -8,6 +8,8 @@
 #include "tap/motor/servo.hpp"
 #include "util/Pose2d.hpp"
 
+#include "subsystems/gimbal/GimbalSubsystem.hpp"
+
 #include "drivers.hpp"
 
 using namespace communication;
@@ -72,7 +74,24 @@ struct PoseData
 
 struct RefSysMsg
 {
+    uint8_t gameStage;
+    uint16_t stageTimeRemaining;
+    uint16_t robotHp;
+    uint8_t robotID; //if was on red team, so hero will always be 1 and not 101
+    float deltaAngleGotHitIn; //if we are looking in a certain direction and get hit in the left, this would be PI/2
+
+    // uint8_t booleans;
+    bool isOnBlueTeam;
+    bool isHealing;
+    bool isInReloadZone;
+    bool isInCenterZone;
+    bool doesTeamOccupyCenterZone;
+    bool doesOpponentTeamOccupyCenterZone;
+    bool doesChassisHavePower;
+    bool doesGimbalHavePower;
 } modm_packed;
+
+static constexpr int test = sizeof(struct RefSysMsg);
 
 // ==== struct type to enum mapping ===
 template<typename T>
@@ -90,6 +109,11 @@ struct PanelData {
 class JetsonSubsystem : public tap::control::Subsystem {
 private:  // Private Variables
     src::Drivers* drivers;
+    GimbalSubsystem* gimbal;
+
+    tap::arch::MilliTimeout refDataSendingTimeout;
+    // bool needToSendRefData = false;
+    static constexpr int TIME_FOR_REF_DATA = 100; //send at 10hz
 
 
     float q0, q1, q2, q3; //easier to convert frames of reference from the quatrenion directly
@@ -105,7 +129,7 @@ private:  // Private Variables
     float posXrelPitch, posYrelPitch, posZrelPitch; //position of panel relative to frame 2 but offset up
     float velXrelPitch, velYrelPitch, velZrelPitch;
 public:  // Public Methods
-    JetsonSubsystem(src::Drivers* drivers);
+    JetsonSubsystem(src::Drivers* drivers, GimbalSubsystem* gimbal);
 
     ~JetsonSubsystem() {}
 
--- MCB-project/src/subsystems/jetson/JetsonSubsystemConstants.hpp
+++ MCB-project/src/subsystems/jetson/JetsonSubsystemConstants.hpp
@@ -7,31 +7,31 @@
 //distance to the camera in frame 4
 //-.0175
 
-float cameraXoffset = -.0325; //appears to be RGB offset, not depth offset
-float cameraYoffset = .1279;
-float cameraZoffset = .04505;
+static constexpr float cameraXoffset = -.0325; //appears to be RGB offset, not depth offset
+static constexpr float cameraYoffset = .1279;
+static constexpr float cameraZoffset = .04505;
 
 
-const float J = 24.0;           // Shot velocity
+static constexpr float initialShotVelocity = 24.0;           // Shot velocity
 
-const float l = 0.05;           // Combined camera + Jetson latency
-const float deltaTime = 0.033;  // Frame time
-const float H = 9.95;   
+static constexpr float l = 0.05;           // Combined camera + Jetson latency
+static constexpr float deltaTime = 0.033;  // Frame time
+static constexpr float H = 9.95;   
 
 #elif defined(SENTRY)
 
 //distance to the camera in frame 4
 //-.0175
-float cameraXoffset = -.0325; //appears to be RGB offset, not depth offset
-float cameraYoffset = .16034;
-float cameraZoffset = .1295;
+static constexpr float cameraXoffset = -.0325; //appears to be RGB offset, not depth offset
+static constexpr float cameraYoffset = .16034;
+static constexpr float cameraZoffset = .1295;
 
 
-const float J = 22.0;           // Shot velocity
+static constexpr float initialShotVelocity = 22.0;           // Shot velocity
 
-const float l = 0.05;           // Combined camera + Jetson latency
-const float deltaTime = 0.033;  // Frame time
-const float H = 9.95;   
+static constexpr float l = 0.05;           // Combined camera + Jetson latency
+static constexpr float deltaTime = 0.033;  // Frame time
+static constexpr float H = 9.95;   
 
 
 
@@ -39,31 +39,31 @@ const float H = 9.95;
 
 //distance to the camera in frame 4
 //-.0175
-float cameraXoffset = -.0325; //appears to be RGB offset, not depth offset
-float cameraYoffset = .180857;
-float cameraZoffset = .048;
+static constexpr float cameraXoffset = -.0325; //appears to be RGB offset, not depth offset
+static constexpr float cameraYoffset = .180857;
+static constexpr float cameraZoffset = .048;
 
 
-const float J = 12.5;           // Shot velocity
+static constexpr float initialShotVelocity = 12.5;           // Shot velocity
 
-const float l = 0.05;           // Combined camera + Jetson latency
-const float deltaTime = 0.033;  // Frame time
-const float H = 9.95;   
+static constexpr float l = 0.05;           // Combined camera + Jetson latency
+static constexpr float deltaTime = 0.033;  // Frame time
+static constexpr float H = 9.95;   
 
 
 
 #else
 
-float cameraXoffset = -.0325; //appears to be RGB offset, not depth offset
-float cameraYoffset = .1279;
-float cameraZoffset = .04505;
+static constexpr float cameraXoffset = -.0325; //appears to be RGB offset, not depth offset
+static constexpr float cameraYoffset = .1279;
+static constexpr float cameraZoffset = .04505;
 
 
-const float J = 24.0;           // Shot velocity
+static constexpr float initialShotVelocity = 24.0;           // Shot velocity
 
-const float l = 0.05;           // Combined camera + Jetson latency
-const float deltaTime = 0.033;  // Frame time
-const float H = 9.95;   
+static constexpr float l = 0.05;           // Combined camera + Jetson latency
+static constexpr float deltaTime = 0.033;  // Frame time
+static constexpr float H = 9.95;   
 
 
 #endif
