0013c0c3e205289ecf6d3d7f0db6a4ce49979121
rhit-hardenej <hardenej@rose-hulman.edu>
2025-06-13 17:23:49 -0400

    terminalSerial is unnecessary for ui

--- MCB-project/src/main.cpp
+++ MCB-project/src/main.cpp
@@ -43,7 +43,7 @@ static void initializeIo(src::Drivers *drivers) {
     drivers->uart.initialize();
 
 
-    drivers->terminalSerial.initialize(); //needs to be commented for cv to work?
+    // drivers->terminalSerial.initialize(); //interferes with jetson because uses the same uart port. Previously believed to be necessary for ui to work, turns out it isn't 
     drivers->schedulerTerminalHandler.init();
     drivers->djiMotorTerminalSerialHandler.init();
     
@@ -108,7 +108,7 @@ int main() {
 
             drivers.djiMotorTxHandler.encodeAndSendCanData();
 
-            drivers.terminalSerial.update(); 
+            // drivers.terminalSerial.update(); 
         } 
 
         // prevent looping too fast
