cd22f8dad5c30dbcf8430d3a60f680c8037a7181
rhit-hardenej <hardenej@rose-hulman.edu>
2025-06-14 13:15:19 -0400

    predicted shots remaining has special behavior for 1v1

--- MCB-project/src/subsystems/ui/PredictedRemainingShotsIndicator.hpp
+++ MCB-project/src/subsystems/ui/PredictedRemainingShotsIndicator.hpp
@@ -13,32 +13,47 @@ public:
     PredictedRemainingShotsIndicator(tap::Drivers* drivers, IndexerSubsystem* index) : drivers(drivers), index(index) { addGraphicsObject(&arc); }
 
     void update() {
-        // check if reloaded
-        tap::communication::serial::RefSerial::Rx::RobotData robotData = drivers->refSerial.getRobotData();
         if (drivers->refSerial.getRefSerialReceivingData()) {
+            tap::communication::serial::RefSerial::Rx::RobotData robotData = drivers->refSerial.getRobotData();
+
+            //check if reloaded
+            bool isReloaded = false;
             if (robotData.rfidStatus.any(RefSerialData::Rx::RFIDActivationStatus::RESTORATION_ZONE | RefSerialData::Rx::RFIDActivationStatus::EXCHANGE_ZONE)) {
-                // not sure if it is the restoration or exchange zone that is what we use in 3v3
+                // not sure if it is the restoration or exchange zone that is what we use in 3v3, should be one of them
                 shotsAtLastReload = index->getTotalNumBallsShot();
+                isReloaded = true;
             }
 
             float shotsShot = index->getTotalNumBallsShot() + shotsAtLastReload;
+
             // shots can be negative (ref system isn't able to cut off power in time to prevent shots), taproot should change the uint to int
+            #if defined(HERO)
+            int16_t shotsBought = robotData.turret.bulletsRemaining42;
+            #else
             int16_t shotsBought = robotData.turret.bulletsRemaining17;
-            if (shotsBought == 0) 
-                shotsBought = robotData.turret.bulletsRemaining42;
+            #endif
+
+            float maxShots = FILLED_NUM_SHOTS;
+            if(drivers->refSerial.getGameData().gameType == RefSerialData::Rx::GameType::ROBOMASTER_RMUL_1V1){
+                maxShots = MAX_SHOTS_1v1;
+            }
 
             if (shotsBought < 0) {
                 // shotsShot is lower, you have shot more than you have bought
                 // shotsBought shots is higher
-                arc.setLower((FILLED_NUM_SHOTS - shotsShot)/FILLED_NUM_SHOTS);
-                arc.setHigher((FILLED_NUM_SHOTS - shotsShot - shotsBought)/FILLED_NUM_SHOTS);
-                arc.color = UISubsystem::Color::GREEN;
+                arc.setLower((maxShots - shotsShot)/maxShots);
+                arc.setHigher((maxShots - shotsShot - shotsBought)/maxShots);
+                arc.color = UISubsystem::Color::PINK;
             } else {
                 // shotsShot is higher, haven't shot more than you have bought
                 // shotsBought shots is lower
-                arc.setHigher((FILLED_NUM_SHOTS - shotsShot)/FILLED_NUM_SHOTS);
-                arc.setLower((FILLED_NUM_SHOTS - shotsShot - shotsBought)/FILLED_NUM_SHOTS);
-                arc.color = UISubsystem::Color::PINK;
+                arc.setHigher((maxShots - shotsShot)/maxShots);
+                arc.setLower((maxShots - shotsShot - shotsBought)/maxShots);
+                arc.color = UISubsystem::Color::GREEN;
+            }
+
+            if(isReloaded){
+                arc.color = UISubsystem::Color::WHITE;
             }
         }
 
@@ -56,6 +71,7 @@ private:
     static constexpr uint16_t SIZE = 392;         // pixels, makes it so we are just inside the left parenthesis thingy
 
     static constexpr int FILLED_NUM_SHOTS = 500;  // this would be ifdefed, but only infantry draws it right now
+    static constexpr int MAX_SHOTS_1v1 = 200;     // if in 1v1, then you are a standard, and you can shoot only 200
     float shotsAtLastReload = 0;                  // when we enter the reload zone, assume we fill completely
 
     LargeCenteredArc arc{false, 0};  // arc on the right size in first lane
