04d627b2c5310bc686553e64cf7ea558c2327c56
rhit-hardenej <hardenej@rose-hulman.edu>
2026-04-05 10:39:17 -0400

    a

--- MCB-project/src/subsystems/ui/objects/ReuseTest.hpp
+++ MCB-project/src/subsystems/ui/objects/ReuseTest.hpp
@@ -14,6 +14,7 @@ public:
         addGraphicsObject(&square);
         addGraphicsObject(&circle);
         addGraphicsObject(&string);
+        setHidden(0);
     }
 
     void update() {
@@ -25,15 +26,18 @@ public:
             prev=true;
             i++;
             if(i==3) i=0;
+            setHidden(i);
         }
-        
-        square.setHidden(i!=0);
-        circle.setHidden(i!=1);
-        string.setHidden(i!=2);
     }
 
 private:
     tap::Drivers* drivers;
+    
+    void setHidden(int i){
+        square.setHidden(i!=0);
+        circle.setHidden(i!=1);
+        string.setHidden(i!=2);
+    }
     
     bool prev = false;
     
@@ -43,4 +47,55 @@ private:
     UnfilledCircle circle{UISubsystem::Color::WHITE, 1000, 700, 20, 5};
     StringGraphic string{UISubsystem::Color::WHITE, "Str", UISubsystem::HALF_SCREEN_WIDTH, 650, 20, 3};
 
+};
+
+
+
+class ReuseTest2 : public GraphicsContainer {
+public:
+    ReuseTest2(tap::Drivers* drivers) : drivers(drivers) {
+        for(int i=0; i<7; i++){
+            squareContainer.addGraphicsObject(squares+i);
+            circleContainer.addGraphicsObject(circles+i);
+            squares[i].x = 700+20*i;
+            squares[i].y = 700;
+            squares[i].width=(squares[i].height=10);
+            squares[i].thickness=(circles[i].r=(circles[i].thickness=5));
+            circles[i].cx=705+20*i;
+            circles[i].cy=725;
+        }
+        addGraphicsObject(&squareContainer);
+        addGraphicsObject(&circleContainer);
+        setHidden(0);
+    }
+
+    void update() {
+        bool f = drivers->remote.keyPressed(Remote::Key::F);
+        if(prev && !f) {
+            prev=false;
+        }
+        if(!prev && f){
+            prev=true;
+            i++;
+            if(i==2) i=0;
+            setHidden(i);
+        }
+    }
+
+private:
+    tap::Drivers* drivers;
+    
+    void setHidden(int i){
+        squareContainer.setHidden(i!=0);
+        circleContainer.setHidden(i!=1);
+    }
+    
+    bool prev = false;
+    
+    int i=0;
+
+    UnfilledRectangle squares[7];
+    UnfilledCircle circles[7];
+    GraphicsContainer squareContainer;
+    GraphicsContainer circleContainer;
 };
