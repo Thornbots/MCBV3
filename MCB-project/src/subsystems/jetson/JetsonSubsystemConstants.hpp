b65a0643230b01d51706bc8aa523cc5d4e3ba26a
rhit-hardenej <hardenej@rose-hulman.edu>
2026-07-03 19:20:52 -0400

    yaw proportional multiply, pitch in progress

--- MCB-project/src/subsystems/jetson/JetsonSubsystemConstants.hpp
+++ MCB-project/src/subsystems/jetson/JetsonSubsystemConstants.hpp
@@ -26,12 +26,15 @@ static constexpr float initialShotVelocity = 24.0;           // Shot velocity
 // static constexpr float deltaTime = 0.0166;  // Frame time
 static constexpr float H = 9.95;   
 
-//here tune pitch
-static constexpr float PITCH_DIVIDE = 1.5f; //higher number is less responsive
+//here tune pitch. dyaw gets scaled by an amount proportional to dyaw
+static constexpr float PITCH_MULTIPLY_MIN = 0.0f;
+static constexpr float PITCH_MULTIPLY_MAX = 0.5f;
+static constexpr float PITCH_MULTIPLY_SCALE = 0.5f; //mult agains dyaw (in rad, max is pi)
 
-//here tune yaw
-static constexpr float YAW_DIVIDE_FAR = 2.0f; //higher number is less responsive
-static constexpr float YAW_DIVIDE_CLOSE = 0.1f; //higher number is less responsive
+//here tune yaw. dyaw gets scaled by an amount proportional to dyaw
+static constexpr float YAW_MULTIPLY_MIN = 0.0f;
+static constexpr float YAW_MULTIPLY_MAX = 0.5f;
+static constexpr float YAW_MULTIPLY_SCALE = 0.5f; //mult agains dyaw (in rad, max is pi)
 
 #elif defined(SENTRY)
 
