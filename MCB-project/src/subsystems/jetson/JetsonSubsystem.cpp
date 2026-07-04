6108fba3a60f6b4619ba8cdea92bcac6970ffef9
1198159 <jeffastedman@gmail.com>
2026-07-04 14:55:21 -0400

    3rd times the charm

--- MCB-project/src/subsystems/jetson/JetsonSubsystem.cpp
+++ MCB-project/src/subsystems/jetson/JetsonSubsystem.cpp
@@ -265,7 +265,7 @@ void JetsonSubsystem::update(
     }
 
     *yawOut = (targetYaw - current_yaw);  // fmod(current_yaw + targetYaw, 2 * PI);
-    *pitchOut = -targetPitch;
+    *pitchOut = targetPitch;
     *yawVelOut = (-cos_theta3 * velXrelPitch - sin_theta3 * velYrelPitch + velXrel4) / (cos_theta4 * posYrel4 - sin_theta4 * posZrel4);
     *pitchVelOut = 0; //unsure on how to compute this well and i dont trust AI
 
