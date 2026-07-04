806e70278c769f0681a9f846b52a2f95133a94ea
1198159 <jeffastedman@gmail.com>
2026-07-03 21:56:36 -0400

    velocity whoops

--- MCB-project/src/subsystems/gimbal/GimbalSubsystem.cpp
+++ MCB-project/src/subsystems/gimbal/GimbalSubsystem.cpp
@@ -135,13 +135,14 @@ void GimbalSubsystem::reZeroYaw() {
 void GimbalSubsystem::setAngles(float yawAngle, float pitchAngle) {
     prevTargetPitch = std::clamp(pitchAngle, -MAX_PITCH_DOWN, MAX_PITCH_UP);
     float pitch = getPitchEncoderValue();
+    float pitchVel = getPitchVel();
 
     driveTrainEncoder = getYawEncoderValue();
     yawEncoderCache = driveTrainEncoder;
     targetYawAngleWorld = yawAngle;  // std::fmod(targetYawAngleWorld + changeInTargetYaw, 2 * PI);
 
     // THIS LINE BELOW WAS CAUSING ERROR
-    pitchMotorVoltage = getPitchVoltage(prevTargetPitch, pitch, 0, dt);
+    pitchMotorVoltage = getPitchVoltage(prevTargetPitch, pitch, pitchVel, dt);
     
     // THIS LINE BELOW WAS CAUSING ERROR
 
